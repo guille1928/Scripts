183b5601c05f8e1ce018e038ce7a8ced26038943
Guille <150677095+guille1928@users.noreply.github.com>
2025-03-05 10:57:33 +0100

    update fotos

--- comprimirFotos.py
+++ comprimirFotos.py
@@ -13,11 +13,32 @@ ruta_completa = os.listdir(directorioRaiz)
 
 extension_foto = ".jpg",".png",".jpeg"
 
+
+#funcion para redimensionar la imagen respetando relacion aspecto
+
+def redimensionar_relacion (imagen, alto, ancho) :
+    #obtengo las dimensiones originales
+    image_ancho , image_alto = imagen.size
+
+ # Calcular la relación de aspecto
+    relacion = min(ancho/image_ancho  , alto/image_alto)
+
+    #nueva size
+    new_size_respetando_ancho = int(image_ancho*relacion)
+    new_size_respetando_alto = int(image_alto*relacion)
+    imagen_modificada = imagen.resize((new_size_respetando_ancho,new_size_respetando_alto), Image.LANCZOS)
+    # Crear una imagen nueva con fondo blanco y las dimensiones finales
+    nueva_imagen = Image.new("RGB", (ancho, alto), (255,255,255))  # Fondo blanco
+    
+    # Pegar la imagen redimensionada en el centro de la nueva imagen
+    nueva_imagen.paste(imagen_modificada, ((ancho - new_size_respetando_ancho) // 2, (alto - new_size_respetando_alto) // 2))
+    return nueva_imagen
+
 for archivos in ruta_completa :
     print("las fotos son : ",archivos)
     ruta_origen = os.path.join(directorioRaiz,archivos)
     ruta_salida = os.path.join(directorioFinal,archivos)
     img= Image.open(ruta_origen)
-    img_resized = img.resize((500 , 300), Image.LANCZOS)
-    img_resized.save(ruta_salida, quality=100,optimize=True)
+    img_redimensionada= redimensionar_relacion(img,264,414)
+    img_redimensionada.save(ruta_salida, format="PNG")
    
